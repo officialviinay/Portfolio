041d65d328bf5dcdf3d64e584aa8294bcdde6339
Vinay kumar <102797675+officialviinay@users.noreply.github.com>
2024-02-17 11:01:31 +0000

    updats

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
+
+
+
+
+// scroll sections
 window.onscroll = () => {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+
+//sticky header
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+// remove toggle icon and navbar when click navbar Links (scroll)
+
+menuIcon.classList.remove('bx-x');
+navBar.classList.remove('active');
